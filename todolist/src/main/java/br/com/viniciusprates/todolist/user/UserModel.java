fd1162432940b09e1b48aebccb5fdbcaaa39fa57
vinicius-prates <viniprates2211@gmail.com>
2023-10-10 16:31:38 -0300

    setup runtime DB

--- todolist/src/main/java/br/com/viniciusprates/todolist/user/UserModel.java
+++ todolist/src/main/java/br/com/viniciusprates/todolist/user/UserModel.java
@@ -1,33 +1,37 @@
 package br.com.viniciusprates.todolist.user;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+// Geralmente quando queremos pegar/mudar os dados, 
+// nos utilizamos os getters/setters respectivamente.
+// Com a lib @lombok, nao eh necessario incluir os getters/setters, basta colocar o @Data, como foi feito abaixo.
+
+// Para usar o lombok em apenas um atributo, basta colocar o @Data em cima de um atributo.
+
+@Data
+@Entity(name ="tb_users") //reconhecimento da tabela usuarios no BD sera tb_users
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID") //desa forma o id sera gerado de maneira automatica
+    private UUID id;
+
+
     private String username;
     private String name;
     private String password;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
+    @CreationTimestamp
+    private LocalDateTime createdAt;
     
 }
 
