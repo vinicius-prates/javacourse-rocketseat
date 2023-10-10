b6a9d18d30681fc70539e5e516d917102f31aa08
vinicius-prates <viniprates2211@gmail.com>
2023-10-10 16:05:49 -0300

    getters and setters

--- todolist/src/main/java/br/com/viniciusprates/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/viniciusprates/todolist/user/UserController.java
@@ -16,6 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @PostMapping("/")
     public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.name);
+        System.out.println(userModel.getUsername());
     }
 }
--- todolist/src/main/java/br/com/viniciusprates/todolist/user/UserModel.java
+++ todolist/src/main/java/br/com/viniciusprates/todolist/user/UserModel.java
@@ -1,7 +1,34 @@
 package br.com.viniciusprates.todolist.user;
 
 public class UserModel {
-    public String username;
-    public String name;
-    public String password;
+    private String username;
+    private String name;
+    private String password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
 }
+
+
